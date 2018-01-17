e30b735ce22840f5c60bf403c9db4542cd4114b6
elzup <guild0105@gmail.com>
2018-01-18 08:33:27 +0900

    Reflect node6

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 require("isomorphic-fetch");
 require("es6-promise").polyfill();
 
-module.exports = async (lang, n) => {
-	lang = lang || "en";
-	if (typeof lang !== "string") {
-		throw new TypeError(`Expected a string, got ${typeof lang}`);
-	}
+module.exports = (lang, n) => {
+	return new Promise(function(resolve, reject) {
+		lang = lang || "en";
+		if (typeof lang !== "string") {
+			throw new TypeError(`Expected a string, got ${typeof lang}`);
+		}
 
-	n = n || 1;
-	if (n <= 0 || n > 10) {
-		throw new TypeError(`Expected a -n (1 - 10), got ${n}`);
-	}
+		n = n || 1;
+		if (n <= 0 || n > 10) {
+			throw new TypeError(`Expected a -n (1 - 10), got ${n}`);
+		}
 
-	// rnlimit: API limit 10 or less
-	const url = `http://${lang}.wikipedia.org/w/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=10`;
+		// rnlimit: API limit 10 or less
+		const url = `http://${lang}.wikipedia.org/w/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=10`;
 
-	const res = await fetch(url);
-	const data = await res.json();
-	const words = data.query.random;
-	words.length = n;
-	return words.map(v => v.title);
+		const res = fetch(url)
+			.then(res => res.json())
+			.then(data => {
+				const words = data.query.random;
+				words.length = n;
+				resolve(words.map(v => v.title));
+			});
+	});
 };
