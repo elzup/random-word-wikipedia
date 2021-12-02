ba98375e510dbc1dd9d665362befedb9fa71b33f
elzup <guild0105@gmail.com>
2021-12-02 19:32:39 +0900

    update packages

--- index.test.js
+++ index.test.js
@@ -1,19 +1,22 @@
-import test, { after, before } from "ava";
+import test from "ava";
 
-import execa from "execa";
+import { execaSync } from "execa";
 import fetchMock from "fetch-mock";
 
-const m = require(".");
+import m from "./index.js";
+import fs from "fs";
 
-const mock1en = require("./mocks/mock1.en.json");
-const mock1ja = require("./mocks/mock1.ja.json");
-const mock3ja = require("./mocks/mock3.json");
-const mock7ja = require("./mocks/mock7.json");
-const mock10ja = require("./mocks/mock10.json");
+const read = (path) => JSON.parse(fs.readFileSync(path, "utf8"));
+
+const mock1en = read("./mocks/mock1.en.json");
+const mock1ja = read("./mocks/mock1.ja.json");
+const mock3ja = read("./mocks/mock3.json");
+const mock7ja = read("./mocks/mock7.json");
+const mock10ja = read("./mocks/mock10.json");
 
 const makeUrl = (lang, n) =>
 	`https://${lang}.wikipedia.org/w/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=${n}`;
-before(() => {
+test.before(() => {
 	fetchMock
 		.get(makeUrl("en", 1), { status: 200, body: mock1en })
 		.get(makeUrl("ja", 1), { status: 200, body: mock1ja })
@@ -21,30 +24,27 @@ before(() => {
 		.get(makeUrl("ja", 7), { status: 200, body: mock7ja })
 		.get(makeUrl("ja", 10), { status: 200, body: mock10ja });
 });
-after(() => {
+test.after(() => {
 	fetchMock.restore();
 });
 
 test("CLI works", async (t) => {
-	const { stdout, code } = await execa.shell("./cli.js");
-	t.is(code, 0);
+	const { stdout } = execaSync("./cli.js");
 	t.is(stdout.split("\n").length, 1);
 });
 
 test("CLI works another lang", async (t) => {
-	const { stdout, code } = await execa.shell("./cli.js ja");
-	t.is(code, 0);
+	const { stdout } = execaSync("./cli.js", ["ja"]);
 	t.is(stdout.split("\n").length, 1);
 });
 
 test("CLI works num option", async (t) => {
-	const { stdout, code } = await execa.shell("./cli.js ja -n 7");
-	t.is(code, 0);
+	const { stdout } = execaSync("./cli.js", ["ja", "-n", "7"]);
 	t.is(stdout.split("\n").length, 7);
 });
 
 test("CLI module throw num limit works", async (t) => {
-	const { stdout, stderr, code } = await execa.shell("./cli.js ja -n 11");
+	const { stderr } = execaSync("./cli.js", ["ja", "-n", "11"]);
 	t.truthy(/Error/.test(stderr));
 });
 
@@ -67,6 +67,6 @@ test("module works num option", async (t) => {
 });
 
 test("module throw num limit works", async (t) => {
-	const err = await t.throwsAsync(m("ja", 11), TypeError);
+	const err = await t.throwsAsync(m("ja", 11), { instanceOf: TypeError });
 	t.truthy(err.message);
 });
